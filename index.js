d8b85be73c4c584a27d3d58ab3c4072abcaa9b79
raymond shawn <76945071+raymondshawn@users.noreply.github.com>
2022-12-08 01:30:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const c = console.log.bind(document);
+
+const startbtn = document.getElementById("start");
+const timer = document.getElementById("timer");
+
+startbtn.addEventListener("click", () => {
+  
+});
